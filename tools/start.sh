1b3c4d5ee775bd0fcb05ee9222133ee6bf4abfad
gaeljw <gjourdanweil@gmail.com>
2016-05-07 21:31:32 +0200

    Update start/stop scripts

--- tools/start.sh
+++ tools/start.sh
@@ -1,1 +1,3 @@
-nohup java -jar bin/domoticz2mongo.jar --spring.config.location=bin/application.properties --logging.config=bin/logback.xml &
+DOMAINE=/apps/domoticz2mongo
+nohup java -jar $DOMAINE/bin/domoticz2mongo.jar --spring.config.location=$DOMAINE/bin/application.properties --logging.config=$DOMAINE/bin/logback.xml &
+echo $! > $DOMAINE/domoticz2mongo.pid
---
+++ tools/stop.sh
@@ -0,0 +1,2 @@
+DOMAINE=/apps/domoticz2mongo
+kill -9 `cat $DOMAINE/domoticz2mongo.pid`
